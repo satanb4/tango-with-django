d5303358e128c20225be6e580b3bb22d56a3700d
satanb4 <sayan.bandyopadhyay@yahoo.co.in>
2023-03-10 19:23:01 +0000

    Added database and generated models with dummy data

--- tango_django_with_project/urls.py
+++ tango_django_with_project/urls.py
@@ -25,5 +25,3 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-print(urlpatterns)
